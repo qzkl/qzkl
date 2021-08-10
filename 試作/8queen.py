cc6191d105b78b4cb2751667246c7c78432d9f79
nakahashi <qzkl2011@gmail.com>
2021-08-10 16:09:08 +0900

    8queen4

--- 試作/8queen.py
+++ 試作/8queen.py
@@ -1,20 +1,27 @@
 pos=[0]*8
-flag=[False]*8
+flag_a=[False]*8
+flag_b=[False]*15
+flag_c=[False]*15
+x=[]
 
 def put():
+    x.append(1)
+    print(len(x))
     for i in range(8):
+        
         print(f'{pos[i]:2}',end=" ")
+        
     print()
 
 def set(i):
     for j in range(8):
-        if not flag[j]:
+        if not flag_a[j] and not flag_b[7-i+j] and not flag_c[14-i-j]:
             pos[i]=j
             if i==7:
                 put()
             else:
-                flag[j]=True
+                flag_a[j]=flag_b[7-i+j]=flag_c[14-i-j]=True
                 set(i+1)
-                flag[j]=False
+                flag_a[j]=flag_b[7-i+j]=flag_c[14-i-j]=False
 
 set(0)
