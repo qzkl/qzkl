eb09b1d290d926b3a7f27974730ea29616d7902a
nakahashi <qzkl2011@gmail.com>
2021-08-29 23:35:46 +0900

    20210829 2

--- 試作/検証.py
+++ 試作/検証.py
@@ -1,6 +1,31 @@
-def test():
-    a=100
-    return a,200
+class Test:
+    
+    
+    def __init__(self,a):
+        self.a=a
+        self.n=0
+        self.x=[]
+    def tf(self):
+        self.a=self.a-1
+        return self.a
+    
+    def total(self):
+        
+        if self.a<1:
+            self.x.append(self.a)
+            
+            return self.n,self.x
+        else:
+            self.n+=self.a
+            self.x.append(self.a)
+            
+            self.tf()
+            return self.total()
 
-f=test()
-print(f)
+b=Test(10)
+c=Test(20)
+
+print(b.total()[1])
+print(c.total()[0])
+
+
