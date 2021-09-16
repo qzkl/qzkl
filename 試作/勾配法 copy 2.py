e377feff1c9037d8cce31f4c76b1a5a4245764f4
nakahashi <qzkl2011@gmail.com>
2021-09-17 00:17:07 +0900

    20210917

--- 試作/勾配法 copy 2.py
+++ 試作/勾配法 copy 2.py
@@ -2,79 +2,42 @@ import math
 import random
 import matplotlib.pyplot as plt
 
-g=4
-x=[[] for b in range(g)]
-y=[[] for b in range(g)]
-a0=-0.8
-b0=1.9
-nn=[0.05,0.2,0.5,0.8]
-
-
 class Stp:
     def __init__(self,x0,y0,n):
         self.x=x0
         self.y=y0
         self.n=n
+        self.xl=[]
+        self.yl=[]
+        self.xl.append(self.x)
+        self.yl.append(self.y)
 
-    def stpf(self):
-        z_0=math.exp(0.5*a**2+0.2*b**2)
+    
+    def stpf(self,a,b):
+        
         x_0=-1*a*math.exp(0.5*a**2+0.2*b**2)
         y_0=-1*0.4*b*math.exp(0.5*a**2+0.2*b**2)
-        x_1=a+n*x_0
-        y_1=b+n*y_0
-        z_1=math.exp(0.5*x_1**2+0.2*y_1**2)
-        x[i].append(x_1)
-        y[i].append(y_1)
+        x_n=a+self.n*x_0
+        y_n=b+self.n*y_0
+        z_n=math.exp(0.5*x_n**2+0.2*y_n**2)
+        return x_n,y_n,z_n
+
+    def reslut(self):
+        z_0=math.exp(0.5*self.x**2+0.2*self.y**2)
+        x_1=self.x=self.stpf(self.x,self.y)[0]
+        y_1=self.y=self.stpf(self.x,self.y)[1]
+        z_1=self.stpf(self.x,self.y)[2]
+        self.xl.append(x_1)
+        self.yl.append(y_1)
+
         if abs(z_0-z_1)<10**-6:
-            return x_1,y_1,len(x)
+            return self.xl,self.yl,len(self.xl)
         else:
-            return stp(x_1,y_1,n,i)
+            self.stpf(x_1,y_1)
+            return self.reslut()
 
-def stp(a,b,n,i):
-    z_0=math.exp(0.5*a**2+0.2*b**2)
-    x_0=-1*a*math.exp(0.5*a**2+0.2*b**2)
-    y_0=-1*0.4*b*math.exp(0.5*a**2+0.2*b**2)
-    x_1=a+n*x_0
-    y_1=b+n*y_0
-    z_1=math.exp(0.5*x_1**2+0.2*y_1**2)
-    x[i].append(x_1)
-    y[i].append(y_1)
-
-    if abs(z_0-z_1)<10**-6:
-        return x_1,y_1,len(x)
-    else:
-        return stp(x_1,y_1,n,i)
-
-i=0
-for n1 in nn:
-    x[i].append(a0)
-    y[i].append(b0)
-    
-    f=stp(a0,b0,n1,i)
-    i+=1
+d=Stp(-0.9,1.8,0.1)
 
 
-plt.subplots_adjust(wspace=0.4, hspace=0.6)
-ax1=plt.subplot(2,2,1)
-ax2=plt.subplot(2,2,2)
-ax3=plt.subplot(2,2,3)
-ax4=plt.subplot(2,2,4)
-
-aa=[ax1,ax2,ax3,ax4]
-
-i=0
-for ax in aa:
-    ax.set_title(len(x[i]), fontsize = 11)
-    ax.set_xlim(-2,2) #x軸の範囲
-    ax.set_ylim(-2,2) #Y軸の範囲
-    ax.plot(x[i],y[i],"k")
-    i+=1
-
+plt.plot(d.reslut()[0],d.reslut()[1],"k")
 plt.show()
-# ax1.plot(x,a_1,"k")
-
-# # plt.plot(x,P[0],"k")
-# # plt.plot(x,P[1],"b")
-
-
-# plt.show()
--- 試作/検証.py
+++ 試作/検証.py
@@ -1,31 +1,31 @@
-class Test:
-    
-    
-    def __init__(self,a):
-        self.a=a
-        self.n=0
-        self.x=[]
-    def tf(self):
-        self.a=self.a-1
-        return self.a
-    
-    def total(self):
-        
-        if self.a<1:
-            self.x.append(self.a)
-            
-            return self.n,self.x
-        else:
-            self.n+=self.a
-            self.x.append(self.a)
-            
-            self.tf()
-            return self.total()
+import numpy as np
+import math
+import matplotlib.pyplot as plt
 
-b=Test(10)
-c=Test(20)
-
-print(b.total()[1])
-print(c.total()[0])
+x=[]
+y_np=[]
+y_math=[]
 
 
+def tanh_np(x):
+    return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+
+def tanh_math(x):
+    return (math.exp(x)-math.exp(-x))/(math.exp(x)+math.exp(-x))
+
+for i in np.arange(-2.0, 2.0, 0.1):
+    x.append(i)
+    y_np.append(tanh_np(i))
+    y_math.append(tanh_math(i))
+
+ax1=plt.subplot(2,1,1)
+ax2=plt.subplot(2,1,2)
+
+# ax1.set_ylim(0,1) #Y軸の範囲
+ax1.plot(x,y_np,"k")
+ax2.plot(x,y_math,"k")
+
+
+
+
+plt.show()
